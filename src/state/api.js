150f786ec830f1d792a6adea0f5d0b25938924bc
shakir raza <shakirraza737@gmail.com>
2024-10-26 10:50:08 +0300

    initial changes

--- src/state/api.js
+++ src/state/api.js
@@ -19,7 +19,8 @@ export const api = createApi({
       Accept: "application/json",
       "Content-Type": "application/json",
       "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin":
+        "https://main.d1bygvczrsspbr.amplifyapp.com",
     },
   }),
   reducerPath: "adminApi",
