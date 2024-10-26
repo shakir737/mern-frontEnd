2e4ab278d9bf1a87ac8368e46e7fcce536f16689
shakir raza <shakirraza737@gmail.com>
2024-10-26 10:36:21 +0300

    initial changes

--- src/state/api.js
+++ src/state/api.js
@@ -13,7 +13,13 @@ export const api = createApi({
       }
       return headers;
     },
-    // credentials: "include",
+
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+    },
   }),
   reducerPath: "adminApi",
   tagTypes: [
