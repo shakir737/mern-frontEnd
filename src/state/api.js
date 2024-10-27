3b0283f52849229ae3fef5a40770dc108195d072
shakir raza <shakirraza737@gmail.com>
2024-10-27 19:44:28 +0300

    initial

--- src/state/api.js
+++ src/state/api.js
@@ -20,8 +20,6 @@ export const api = createApi({
       Accept: "application/json",
       "Content-Type": "application/json",
       "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     },
   }),
   reducerPath: "adminApi",
